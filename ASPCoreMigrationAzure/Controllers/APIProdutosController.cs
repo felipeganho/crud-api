de424f1ea1836ce4e31522f42f65c006dd57f4eb
Felipe Silva Ganho <felipeganho@gmail.com>
2021-03-10 10:43:19 -0300

    inicial commit

---
+++ ASPCoreMigrationAzure/Controllers/APIProdutosController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASPCoreMigrationAzure.Models;
+
+namespace ASPCoreMigrationAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIProdutosController : ControllerBase
+    {
+        private readonly BancoContexto _context;
+
+        public APIProdutosController(BancoContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/APIProdutos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProduto()
+        {
+            return await _context.Produto.ToListAsync();
+        }
+
+        // GET: api/APIProdutos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produto.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+
+        // PUT: api/APIProdutos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            if (id != produto.ProdutoID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/APIProdutos
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _context.Produto.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", new { id = produto.ProdutoID }, produto);
+        }
+
+        // DELETE: api/APIProdutos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduto(int id)
+        {
+            var produto = await _context.Produto.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produto.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.Produto.Any(e => e.ProdutoID == id);
+        }
+    }
+}
---
+++ ASPCoreMigrationAzure/obj/Release/net5.0/PubTmp/EFSQLScripts/ASPCoreMigrationAzure.Models.BancoContexto.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210310001306_inicial')
+BEGIN
+    CREATE TABLE [Produto] (
+        [ProdutoID] int NOT NULL IDENTITY,
+        [Descricao] nvarchar(max) NULL,
+        [Preco] float NOT NULL,
+        [Qtde] float NOT NULL,
+        CONSTRAINT [PK_Produto] PRIMARY KEY ([ProdutoID])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20210310001306_inicial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20210310001306_inicial', N'5.0.3');
+END;
+GO
+
+COMMIT;
+GO
+
